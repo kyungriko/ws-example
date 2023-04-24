b573df889b3a0b425ef4996436e346c1c90d182a
kyungriko <bydan1128@inswave.com>
2023-04-24 17:04:08 +0900

    example 사이트 크롤링

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+// var express = require("express");
+// var app = express();
+
 const { Builder, By, Key, until } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
+const fs = require("fs");
+const reulstFileName = "result.txt";
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+let contents = new Map();
+
+// app.listen(300, '0.0.0.0', ()=>{
+//     console.log('Server is running : port 3000');
+// });
+
+// map to stirng
+const replacer = function (key, value) {
+  if (value instanceof Map) {
+    //형식 확인
+    return {
+      dataType: "Map", //정의
+      value: Array.from(value.entries()), //entries 함수를 통해 배열로 변경(이중)
+    };
+  } else {
+    return value;
+  }
+};
+
+const service = new chrome.ServiceBuilder(
+  "/ws-example/ws-example/chromedriver.exe"
+);
 
 const run = async () => {
   // headless로 크롬 드라이버 실행
   let driver = await new Builder()
     .forBrowser("chrome")
+    .setChromeService(service)
     .setChromeOptions(
       new chrome.Options()
         .headless()
@@ -14,32 +49,83 @@ const run = async () => {
 
   try {
     // 특정 URL 생성
-    await driver.get("https://map.kakao.com/");
+    await driver.get(
+      "https://example.websquare.kr/websquare/websquare.html?w2xPath=/mainContainer.xml"
+    );
     let userAgent = await driver.executeScript("return navigator.userAgent;");
     console.log("[UserAgent]", userAgent);
 
-    // By.id로 #query Element를 얻어온다.
-    let searchInput = await driver.findElement(By.id("search.keyword.query"));
-    // keyword를 선택하고 검색 버튼 사용
-    let keyword = "닭발";
-    searchInput.sendKeys(keyword, Key.ENTER);
-
-    // css selector로 가져온 element가 위치할때까지 최대 10초간 기다린다.
+    // css selector로 가져온 element가 위치할때까지 최대 20초간 기다린다.
     await driver.wait(
-      until.elementLocated(By.id("info.search.place.list")),
-      10000
+      until.elementLocated(
+        By.className("w2group pos_relative ofh isCollapsed")
+      ),
+      20000
     );
-    let resultElements = await driver.findElements(By.className("placetit"));
 
-    // 검색한 elements 하위의 value를 출력함
-    console.log("[resultElements.length]", resultElements.length);
-    for (var i = 0; i < resultElements.length; i++) {
-      console.log("- " + (await resultElements[i].getCssValue()));
+    // 다 클릭해서 열어준다. (collapsed 상태에서는 text가 읽어지지 않음)
+    let titleButtons = await driver.findElements(
+      By.className("w2textbox fl com_example_left_menu_category_depth1")
+    );
+
+    let idx = 0;
+    for (let button of titleButtons) {
+      // 첫번째 메뉴는 열려있기 때문에 클릭해서 열지 않고, 닫혀 있는 나머지 메뉴들만 클릭해서 열어준다.
+      if (idx != 0) {
+        await button.click();
+      }
+      idx++;
     }
+
+    // 클릭하면 페이지로 이동하는 anchor 요소들
+    let childAnchorElement = await driver.findElements(
+      By.className("w2anchor2 com_example_left_menu_lbl")
+    );
+
+    // contents classname = w2wframe com_example_descBox example_viewer
+    // 검색한 elements 하위의 value를 출력함
+    console.log("[resultElements.length]", childAnchorElement.length);
+    for (var i = 0; i < childAnchorElement.length; i++) {
+      let title = await childAnchorElement[i].getText();
+      console.log("- " + title);
+      try {
+        await childAnchorElement[i].click();
+        // css selector로 가져온 element가 위치할때까지 최대 20초간 기다린다.
+        await driver.wait(
+          until.elementLocated(
+            By.className(
+              "w2wframe w2tabContainer_contents w2tabcontrol_contents_wrapper w2tabcontrol_contents_wrapper_selected"
+            )
+          ),
+          40000
+        );
+
+        let activeTabContainer = await driver.findElements(
+          By.className(
+            "w2wframe w2tabContainer_contents w2tabcontrol_contents_wrapper w2tabcontrol_contents_wrapper_selected"
+          )
+        );
+        let temp = "";
+
+        for (var j = 0; j < activeTabContainer.length; j++) {
+          temp += await activeTabContainer[j].getText();
+        }
+        contents.set(["title", title], ["tmp", temp]);
+      } catch {
+        console.log(title + "은 클릭 상호작용할 수 없는 요소입니다.");
+      }
+    }
+
+    console.log(contents);
+    let resultStr = JSON.stringify(contents, replacer);
+    fs.writeFileSync(reulstFileName, resultStr, (err) => {
+      if (err) throw err;
+    });
   } catch (e) {
     console.log(e);
   } finally {
     driver.quit();
   }
 };
+
 run();
